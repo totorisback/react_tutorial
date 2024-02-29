98791806f9523665eaed6eb285276ab4573c09fe
FJU <frederic.juhel@gmail.com>
2024-02-29 11:47:02 +0100

    gestion du gagnant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
-
+//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
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
   function handleClick(index) {
-    if(squares[index]) {
+    
+    if(squares[index]||calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -33,6 +41,7 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+      <div className="status">{status}</div>
     </>
   );
 }
@@ -43,4 +52,25 @@ function Square({ value, onSquareClick }) {
       {value}
     </button>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
